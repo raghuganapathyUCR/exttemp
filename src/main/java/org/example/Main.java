c08d953e3a01a789651675d2ca4599ec869e264c
Raghu Ganapathy <rgana001@ucr.edu>
2023-04-26 10:28:11 -0700

    From Catchup04- Added changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.annotation.Nullable;
+
+public class Main {
+    static void log(Object x) {
+        System.out.println(x.toString());
+    }
+    static void foo() {
+        log(null);
+    }
+
+    public static void main(String[] args) {
+        foo();
+    }
+}
